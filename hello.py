d67dae27bc02cf015224f899ff52f549a057baeb
Principle <liuyao111111@vip.qq.com>
2015-07-16 19:39:33 +0800

    add

--- hello.py
+++ hello.py
@@ -1,16 +1,23 @@
 from flask import Flask
 from flask import request
+from flask import make_response
+#from flask.ext.script import Manager
 
 app = Flask(__name__)
-
+#manager = Manager(app)
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is %s</p>' % user_agent
+    response = make_response('<h1>This document carries a cookie</h1>')
+    response.set_cookie('answer', "42")
+    return response
 
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello,%s!</h1>' % name
+@app.before_first_request
+def before():
+    return 'header'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=9527)
+#    manager.run()
